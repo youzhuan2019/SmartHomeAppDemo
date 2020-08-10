37373af1858bb6d25d9a8421bee5f205eeac380d
ljx <1296766433@qq.com>
2020-08-10 11:59:08 +0800

    Demo

--- demoservcie/src/test/java/com/youzhuan/demoservcie/ExampleUnitTest.java
+++ demoservcie/src/test/java/com/youzhuan/demoservcie/ExampleUnitTest.java
@@ -1,5 +1,8 @@
 package com.youzhuan.demoservcie;
 
+import com.google.gson.Gson;
+import com.google.gson.internal.bind.ReflectiveTypeAdapterFactory;
+
 import org.junit.Test;
 
 import static org.junit.Assert.*;
@@ -13,5 +16,41 @@ public class ExampleUnitTest {
     @Test
     public void addition_isCorrect() {
         assertEquals(4, 2 + 2);
+    }
+
+    @Test
+    public void testGson() {
+        Gson gson = new Gson();
+        Root root = gson.fromJson("{\"name\":\"so json 在线工具\",\"age\":11}",Root.class);
+        System.out.println(root.toString());
+    }
+
+    public static class Root {
+        private String name;
+
+        private int age;
+
+        public void setName(String name){
+            System.out.println("setName");
+            this.name = name;
+        }
+        public String getName(){
+            return this.name;
+        }
+        public void setAge(int age){
+            System.out.println("setAge");
+            this.age = age;
+        }
+        public int getAge(){
+            return this.age;
+        }
+
+        @Override
+        public String toString() {
+            return "Root{" +
+                    "name='" + name + '\'' +
+                    ", age=" + age +
+                    '}';
+        }
     }
 }
